b4b896a8a15b16183fd38cd68d36c88c95d59cac
Phoomparin Mano <phoomparin@gmail.com>
2019-02-08 16:12:58 +0700

    :lipstick: Update Favicon

--- src/components/Document.js
+++ src/components/Document.js
@@ -7,6 +7,11 @@ export function Document(props) {
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
 
+        <link rel="apple-touch-icon" href="/logo/apple-touch-icon.png">
+        <link rel="icon" type="image/png" href="/logo/favicon-16x16.png" sizes="16x16" />
+        <link rel="icon" type="image/png" href="/logo/favicon-32x32.png" sizes="32x32" />
+        <link rel="icon" type="image/png" href="/logo/favicon-128x128.png" sizes="128x128" />
+        <link rel="icon" type="image/png" href="/logo/favicon-196x196.png" sizes="196x196" />
         <link rel="manifest" href="/manifest.json" />
 
         <title>สมัครเข้าค่าย Young Creator's Camp #ค่ายเด็กกล้าสร้าง</title>
