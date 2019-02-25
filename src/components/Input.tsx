6c0348bce91c801e14749ffdf90824f4afe67516
Chanissa Trithipkaiwanpon <chanissat@Chanissas-MacBook-Pro-2.local>
2019-02-25 19:07:32 +0700

    add box-shadow input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -39,9 +39,9 @@ export const TextInput = styled.input<{meta: IMeta}>`
     color: #E0E0E0;
   }
 
-  // &:hover:enabled {
-  //   box-shadow: 0 3px 18.5px 2px rgba(0, 0, 0, 0.18);
-  // }
+  &:hover:enabled {
+    box-shadow: 0 3px 18.5px 2px rgba(0, 0, 0, 0.18);
+  }
 
   &:focus,
   &:active:enabled {
